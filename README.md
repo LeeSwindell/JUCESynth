990b704419f7d09f741f04c120c226f909a96cca
Lee Swindell <leeaswindell@gmail.com>
2022-02-16 10:09:51 -0500

    synth started from the audio programmer tutorial

---
+++ README.md
@@ -0,0 +1,1 @@
+Starting a basic synthesizer plugin using juce.
---
+++ Source/SynthSound.h
@@ -0,0 +1,20 @@
+/*
+  ==============================================================================
+
+    SynthSound.h
+    Created: 16 Feb 2022 9:53:32am
+    Author:  Lee Swindell
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+class SynthSound : public juce::SynthesiserSound
+{
+public:
+    bool appliesToNote(int midiNoteNumber) override { return true };
+    bool appliesToChannel(int midiChannel) override { return true };
+};
